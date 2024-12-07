e1df4250287109740d782def46a1d5fc2eda368b
moham <moham@MOHAMED>
2024-12-07 20:13:18 +0200

    Add project files.

---
+++ Cs Basics/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Cs_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Qst 1
+
+            Console.WriteLine("Please Enter Your Number:");
+
+            // Read the input and convert it to an integer
+            if (int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Your number is {0}", input);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+            #endregion
+        }
+    }
+}
